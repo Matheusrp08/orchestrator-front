d2d8d7d42eec6c6df66036f548a9c56e59616e0b
Matheus de Padua <matheus.padua@outlook.com>
2025-12-15 00:02:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa'; // Opcional, se usar PWA
 
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({}), // Descomente se usar PWA
+    // Remova ou comente esta linha:
+    // VitePWA({})
   ],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    }
   },
-  server: {
-    port: 3000,
-    strictPort: true
-  }
 });
